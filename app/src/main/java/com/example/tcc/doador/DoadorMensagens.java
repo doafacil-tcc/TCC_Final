3b3d4e9a500ae6e55b59df571a9f9ba424ad00c4
btcorso <uniebcorso@fei.edu.br>
2020-11-08 20:01:31 -0300

    Preparado os XML do CHAT

---
+++ app/src/main/java/com/example/tcc/doador/DoadorMensagens.java
@@ -0,0 +1,16 @@
+package com.example.tcc.doador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tcc.R;
+
+public class DoadorMensagens extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.doador_activity_mensagens);
+    }
+}
---
+++ app/src/main/java/com/example/tcc/ong/OngChat.java
@@ -0,0 +1,16 @@
+package com.example.tcc.ong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tcc.R;
+
+public class OngChat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ong_activity_chat);
+    }
+}
